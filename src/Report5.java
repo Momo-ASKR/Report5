8efee597f9fc21cddbd85f99af20851d7ad57512
Daiki Gushiken <e165742@ie.u-ryukyu.ac.jp>
2016-12-05 17:30:13 +0900

    report5

---
+++ src/Report5.java
@@ -0,0 +1,9 @@
+/**
+ * Created by e165742 on 2016/12/05.
+ */
+public class Report5 {
+    public static void main(String args[]) {
+        String str = null;
+        System.out.print(str.length());
+    }
+}
