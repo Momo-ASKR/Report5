9fa35bfacc1c16d3002e3983cb5705ad69996b40
Daiki Gushiken <e165742@ie.u-ryukyu.ac.jp>
2016-12-07 23:15:18 +0900

    report5 step3

--- src/Report5.java
+++ src/Report5.java
@@ -3,11 +3,15 @@
  */
 public class Report5 {
     public static void main(String args[]) {
+        double value;
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            value=Double.parseDouble(str);
         } catch (NullPointerException e) {
             System.out.println("NullPointerExceptionが発生した");
+            e.printStackTrace();
+        } catch (NumberFormatException e){
+            System.out.println("NumberFormatExceptionが発生した");
             e.printStackTrace();
         }
     }
